cb9a5bb29e2fa6bfeb6f02c1886fdd592efd0ccd
unknown <mjmaravillo@gmail.com>
2023-02-04 17:11:05 +0800

    Added message if records are empty.

--- resources/views/registrations/index.blade.php
+++ resources/views/registrations/index.blade.php
@@ -53,130 +53,134 @@
     </div>
 
     <section class="w-11/12 mx-auto font-montserrat">
-        <div class="flex flex-col">
-            <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-                    <div class="overflow-hidden">
-                        <table class="min-w-full border text-center bg-white">
-                            <thead class="border-b">
-                            <tr>
-                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
-                                    Full Name
-                                </th>
-                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
-                                    Type
-                                </th>
-                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
-                                    College
-                                </th>
-                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
-                                    Date Registered
-                                </th>
-                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
-                                    Confirmation
-                                </th>
-                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
-                                    Actions
-                                </th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach ($registrations as $registration)
-                                <tr class="border-b">
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">
-                                        {{ $registration->fullname() }}
-                                    </td>
-                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
-                                        {{ $registration->type }}
-                                    </td>
-                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
-                                        {{ $registration->school->name }}
-                                    </td>
-                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
-                                        {{ $registration->date_registered }}
-                                    </td>
-
-                                    @if($registration->confirmed === 'yes')
-                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
-                                            Confirmed
+        @if(count($registrations) !== 0)
+            <div class="flex flex-col">
+                <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                        <div class="overflow-hidden">
+                            <table class="min-w-full border text-center bg-white">
+                                <thead class="border-b">
+                                <tr>
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+                                        Full Name
+                                    </th>
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+                                        Type
+                                    </th>
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+                                        College
+                                    </th>
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+                                        Date Registered
+                                    </th>
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+                                        Confirmation
+                                    </th>
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+                                        Actions
+                                    </th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach ($registrations as $registration)
+                                    <tr class="border-b">
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">
+                                            {{ $registration->fullname() }}
                                         </td>
-                                    @endif
-
-                                    @if($registration->confirmed === 'no')
                                         <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
-                                            Unconfirmed
+                                            {{ $registration->type }}
                                         </td>
-                                    @endif
+                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                                            {{ $registration->school->name }}
+                                        </td>
+                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                                            {{ $registration->date_registered }}
+                                        </td>
 
-                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                        <div class="flex justify-center space-x-2">
-                                            <button id="{{ $registration->id }}" onclick="updateConfirmation(this)"
-                                                    class="bg-blue-500
+                                        @if($registration->confirmed === 'yes')
+                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                                                Confirmed
+                                            </td>
+                                        @endif
+
+                                        @if($registration->confirmed === 'no')
+                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                                                Unconfirmed
+                                            </td>
+                                        @endif
+
+                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            <div class="flex justify-center space-x-2">
+                                                <button id="{{ $registration->id }}" onclick="updateConfirmation(this)"
+                                                        class="bg-blue-500
                                                         text-white px-3
                                                         py-1
                                                 rounded-md" style="font-weight: 400">
-                                                {{ $registration->confirmed == 'yes' ? 'Unconfirm' : 'Confirm' }}
-                                            </button>
+                                                    {{ $registration->confirmed == 'yes' ? 'Unconfirm' : 'Confirm' }}
+                                                </button>
 
-                                            <form
-                                                id="update-form-{{ $registration->id }}"
-                                                action="{{ route('registrations.update', $registration->id) }}"
-                                                method="POST"
-                                                style="display: none"
-                                            >
-                                                @csrf
-                                                @method('PATCH')
-                                            </form>
+                                                <form
+                                                    id="update-form-{{ $registration->id }}"
+                                                    action="{{ route('registrations.update', $registration->id) }}"
+                                                    method="POST"
+                                                    style="display: none"
+                                                >
+                                                    @csrf
+                                                    @method('PATCH')
+                                                </form>
 
-                                            <button class="bg-red-500 text-white px-3 py-1
+                                                <button class="bg-red-500 text-white px-3 py-1
                                                 rounded-md" style="font-weight: 400" data-modal-target="popup-modal" data-modal-toggle="popup-modal">Delete
-                                            </button>
+                                                </button>
 
-                                            <div id="popup-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden
+                                                <div id="popup-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden
     overflow-y-auto md:inset-0 h-modal md:h-full font-montserrat">
-                                                <div class="relative w-full h-full max-w-md md:h-auto">
-                                                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                                                        <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="popup-modal">
-                                                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                                                            <span class="sr-only">Close modal</span>
-                                                        </button>
-                                                        <div class="p-6 text-center">
-                                                            <svg aria-hidden="true" class="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                                            <h3 class="mb-5 text-lg font-normal text-gray-500
+                                                    <div class="relative w-full h-full max-w-md md:h-auto">
+                                                        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                                            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="popup-modal">
+                                                                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                                                <span class="sr-only">Close modal</span>
+                                                            </button>
+                                                            <div class="p-6 text-center">
+                                                                <svg aria-hidden="true" class="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                                                <h3 class="mb-5 text-lg font-normal text-gray-500
                                                             dark:text-gray-400 whitespace-normal">Are you sure you want to
-                                                                delete this participant?</h3>
-                                                            <button id="{{ $registration->id }}"
-                                                                    data-modal-hide="popup-modal"
-                                                                    type="submit"
-                                                                    onclick="deleteParticipant(this)" class="text-white
+                                                                    delete this participant?</h3>
+                                                                <button id="{{ $registration->id }}"
+                                                                        data-modal-hide="popup-modal"
+                                                                        type="submit"
+                                                                        onclick="deleteParticipant(this)" class="text-white
                                                                     bg-red-600
                                                                     hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-                                                                Yes, I'm sure
-                                                            </button>
-                                                            <button data-modal-hide="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</button>
+                                                                    Yes, I'm sure
+                                                                </button>
+                                                                <button data-modal-hide="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</button>
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </div>
 
-                                            <form
-                                                id="delete-form-{{ $registration->id }}"
-                                                action="{{ route('registrations.destroy', $registration->id) }}"
-                                                method="POST"
-                                            >
-                                                @csrf
-                                                @method('DELETE')
-                                            </form>
-                                        </div>
-                                    </td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
+                                                <form
+                                                    id="delete-form-{{ $registration->id }}"
+                                                    action="{{ route('registrations.destroy', $registration->id) }}"
+                                                    method="POST"
+                                                >
+                                                    @csrf
+                                                    @method('DELETE')
+                                                </form>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        @else
+            <p class="text-white">There are no participant records.</p>
+        @endif
     </section>
 
     <div class="w-11/12 mx-auto mb-5 font-montserrat pb-5">
