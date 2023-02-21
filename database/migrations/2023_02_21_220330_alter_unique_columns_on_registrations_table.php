5487253df255cb900a3e423b1f39f8209ee28a67
unknown <mjmaravillo@gmail.com>
2023-02-21 22:10:36 +0800

    Altered registrations table to filter unique inputs of firstname, lastname, middle_initial and tshirt size.

---
+++ database/migrations/2023_02_21_220330_alter_unique_columns_on_registrations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('registrations', function (Blueprint $table) {
+            $table->unique(['firstname', 'lastname', 'middle_initial', 'tshirt']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
