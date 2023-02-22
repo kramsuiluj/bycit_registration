a9814049a9d0716ad1cfb36303f08b9e9b22dadd
Maleadal <johnmarkpili@gmail.com>
2023-02-22 10:08:59 +0800

    Added count sizes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\RegistrationController;
-use App\Http\Controllers\SessionController;
+use App\Models\Registration;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\RegistrationController;
 
 Route::get('/', [RegistrationController::class, 'create'])->name('registrations.create');
 Route::post('/register', [RegistrationController::class, 'store'])->name('registrations.store');
@@ -18,6 +19,20 @@ Route::group(['middleware' => 'auth'], function () {
         Route::patch('/first/{registration}', [RegistrationController::class, 'updateFirstDay'])->name('updateFirstDay');
         Route::patch('/second/{registration}', [RegistrationController::class, 'updateSecondDay'])->name('updateSecondDay');
         Route::delete('/{registration}/delete', [RegistrationController::class, 'destroy'])->name('destroy');
+        Route::get('/count-sizes', function(){
+            $registrations = Registration::all();
+            $sizes = ['xsmall' => 0,'small' => 0, 'medium' => 0, 'large' => 0, 'xlarge' => 0, 'xxlarge' => 0, 'xxxlarge' => 0];
+            $sizes['xsmall'] = $registrations->where('tshirt', '1')->count();
+            $sizes['small'] = $registrations->where('tshirt', '2')->count();
+            $sizes['medium'] = $registrations->where('tshirt', '3')->count();
+            $sizes['large'] = $registrations->where('tshirt', '4')->count();
+            $sizes['xlarge'] = $registrations->where('tshirt', '5')->count();
+            $sizes['xxlarge'] = $registrations->where('tshirt', '6')->count();
+            $sizes['xxxlarge'] = $registrations->where('tshirt', '7')->count();
+            $sizes['xxxxlarge'] = $registrations->where('tshirt', '8')->count();
+
+            dd($sizes);
+        });
     });
 
     Route::delete('/logout', [SessionController::class, 'logout'])->name('logout');
